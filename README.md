1c72d44e10ec83cfbd4e6424336b6691457130c6
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-12-07 19:24:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install
+
+run the   
+`setup.windows.ps1`
+
+## Running the tests
+
+You can run the tests by running the following command from the exercise directory.
+
+```bash
+$ rebar3 eunit
+```
---
+++ install.windows.ps1
@@ -0,0 +1,22 @@
+# To run this script directly, run this in an elevated admin PowerShell prompt:
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/erlang.starterproject/master/install.windows.ps1 | Invoke-Expression
+
+# This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
+#
+
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+
+choco install erlang --version=23.3
+choco install rebar3
+
+# Clone repo
+& "C:\Program Files\Git\cmd\git.exe" clone https://raw.githubusercontent.com/LearnWithLlew/erlang.starterproject.get C:\Code\erlang.starterproject
+cd C:\Code\erlang.starterproject
+
+# Run starting haskell stuff
+run_tests.bat
+
+# Done
+cls
+echo "Done!"
